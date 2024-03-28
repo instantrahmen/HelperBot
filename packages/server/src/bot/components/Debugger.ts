7cb4e79298ff7f120a3e2e35d57458971ba95052
Erika <eatmyrahmen@gmail.com>
2024-03-27 23:17:07 -0400

    Update debugger

--- packages/server/src/bot/components/Debugger.ts
+++ packages/server/src/bot/components/Debugger.ts
@@ -4,10 +4,10 @@ import { stringify } from '../utils';
 import BaseComponent, { ComponentState } from './BaseComponent';
 export class Debugger extends BaseComponent {
   debugMode = !!process.env.DEBUG_MODE;
-  disableCommands = false;
-  // disableCommands = !!process.env.DEBUG_MODE
-  //   ? false
-  //   : config.environment === NodeEnv.DEVELOPMENT;
+  // disableCommands = false;
+  disableCommands = !!process.env.DEBUG_MODE
+    ? false
+    : config.environment === NodeEnv.DEVELOPMENT;
 
   static inMemoryDebugLog: any[] = [];
 
