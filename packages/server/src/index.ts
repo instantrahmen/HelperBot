9c792d0500afdd459d94ff7030a64b8ae66d1ab0
instantRahmen <erika@cudd.io>
2024-07-16 23:23:26 -0400

    Fix production server not starting

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,14 +1,10 @@
-import { botState } from './bot/components/Bot';
 import { initialize } from './bot';
 import stringify from 'safe-json-stringify';
 import fastify from 'fastify';
 import config from './bot/config';
-import envConfig from '@helper/config';
 
 import { Debugger } from './bot/components/Debugger';
 import commandState from './bot/components/Commands';
-import { Client } from 'discord.js';
-import HelperEmotionsController from './bot/state/emotions-state';
 
 const serializeWithBigInts = (k: string, v: unknown) => {
   if (typeof v === 'bigint') {
@@ -20,11 +16,9 @@ const serializeWithBigInts = (k: string, v: unknown) => {
 const app = async () => {
   const { environment } = config;
   console.log('running in', environment);
-  // console.log('config', config);
 
   const app = fastify({ logger: true });
 
-  console.log('args', process.argv);
   let botState: any;
   if (process.argv.includes('--cleanup')) {
     // clean up
@@ -55,38 +49,5 @@ const app = async () => {
 
   return app;
 };
-// Run the server
-// const start = async () => {
-//   try {
-//     await app.listen({
-//       port: 5000,
-//     });
-//   } catch (err) {
-//     app.log.error(err);
-//     process.exit(1);
-//   }
-// };
-
-// start();
-
-// process.on('SIGINT', async () => {
-//   console.log('Caught interrupt signal');
-//   console.log('Cleaning up and exiting...');
-
-//   console.log({ mpState });
-
-//   console.log('Disconnecting music players...');
-//   Object.keys(mpState.componentsByGuild).forEach((key) => {
-//     mpState.componentsByGuild[key].disconnect();
-//     mpState.componentsByGuild[key].destroy();
-//   });
-
-//   console.log('unregistering commands...');
-
-// await commandState.deployAll(true);
-
-//   console.log('done.');
-//   process.exit();
-// });
 
 export const viteNodeApp = app();
--- packages/server/vite.config.ts
+++ packages/server/vite.config.ts
@@ -6,6 +6,9 @@ import { VitePluginNode } from 'vite-plugin-node';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    ssr: true,
+    outDir: 'dist',
+    emptyOutDir: true,
     commonjsOptions: { transformMixedEsModules: true },
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -15,12 +18,17 @@ export default defineConfig({
   resolve: {
     alias: { src: resolve('src/'), '#root': resolve(__dirname) + '../../' },
   },
+  server: {
+    port: 3000,
+  },
   plugins: [
     dts(),
     ...VitePluginNode({
-      appPath: 'src/index.ts',
+      appPath: './src/index.ts',
       adapter: 'fastify',
-      tsCompiler: 'esbuild',
+      tsCompiler: 'swc',
+      initAppOnBoot: true,
+      exportName: 'viteNodeApp',
     }),
   ],
 }) satisfies UserConfig;
