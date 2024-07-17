7b698c38e71ec4304e1f882ef3f54d756db2072a
instantRahmen <erika@cudd.io>
2024-07-16 22:11:05 -0400

    Guard against null values

--- config/config.js
+++ config/config.js
@@ -1,12 +1,25 @@
 import dotenv from 'dotenv-mono';
-dotenv.config();
+
+if (process.env.NODE_ENV !== 'production' && !!dotenv && !!dotenv.config) {
+  dotenv.config();
+}
+
+/**
+ * Returns the value if it is truthy, otherwise returns an empty string.
+ *
+ * @param {string} value - The input value to check.
+ * @return {string} The value if truthy, otherwise an empty string.
+ */
+const valueOrEmpty = (value) => {
+  return value || '';
+};
 
 const config = {
-  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
-  DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
-  GUILDS: process.env.GUILDS.split(' '),
-  PB_URL: process.env.PB_URL,
+  DISCORD_CLIENT_ID: valueOrEmpty(process.env.DISCORD_CLIENT_ID),
+  DISCORD_CLIENT_SECRET: valueOrEmpty(process.env.DISCORD_CLIENT_SECRET),
+  DISCORD_BOT_TOKEN: valueOrEmpty(process.env.DISCORD_BOT_TOKEN),
+  GUILDS: valueOrEmpty(process.env.GUILDS).split(' '),
+  PB_URL: valueOrEmpty(process.env.PB_URL),
 };
 
 export default config;
